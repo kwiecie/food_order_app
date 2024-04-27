8cecd8181e99c16be035da8aef7d403e0fc9e797
olga <okwiecien10@gmail.com>
2024-04-27 12:30:59 +0200

    Styling fixes

--- src/Meals/MealForm.jsx
+++ src/Meals/MealForm.jsx
@@ -7,6 +7,32 @@ import styles from './MealForm.module.css';
 export default function MealItemForm (props) {
     const quantityInputRef = useRef(); 
     const [quantityIsValid, setQuantityIsValid] = useState(true);
+    const [quantity, setQuantity] = useState(1);
+    const [quantityTooSmall, setQuantityTooSmall] = useState(true);
+
+    const increaseQuantityHandler = () => {
+        if (quantity >= 1) {
+            setQuantityTooSmall(false);
+        }
+        if (quantity >= 5) {
+            setQuantityIsValid(false);
+            return quantity;
+        }
+        setQuantity(quantity + 1);
+        return quantity;
+    }
+
+    const decrementQuantityHandler = () => {
+        if (quantity <= 1) {
+            setQuantityTooSmall(true);
+            return quantity;
+        } if (quantity > 4) {
+            setQuantityIsValid(true);
+        } 
+            setQuantity(quantity - 1);
+            return quantity;
+        
+    };
 
     const submitHandler = event => {
         event.preventDefault();
@@ -26,20 +52,20 @@ export default function MealItemForm (props) {
     }
     return (
         <form className={styles.form} onSubmit={submitHandler}>
-            <Input
-                ref={quantityInputRef}
-                label="Quantity"
-                input={{
-                    id: 'quantity_' + props.id,
-                    type: 'number',
-                    min: '1',
-                    max: '5',
-                    step: '1',
-                    defaultValue: '1',
-                }}
-            />
+            <div className={styles.quantity}>
+                {quantityTooSmall ? <button className={styles['quantity-disabled']} type='button'>-</button> :
+                <button onClick={decrementQuantityHandler} className={styles['quantity-button']} type='button'>-</button>}
+                <input 
+                    type='text' 
+                    value={quantity} 
+                    ref={quantityInputRef} 
+                    id={'quantity_'+ props.id}
+                    className={styles.input}
+                />
+                {!quantityIsValid ? <button className={styles['quantity-disabled']} type='button'>+</button> :
+                <button onClick={increaseQuantityHandler} className={styles['quantity-button']} type='button'>+</button>}
+            </div>
             <Button className={styles.button}>Add</Button>
-            {!quantityIsValid && <p>Please enter a valid quantity (1-5)!</p>}
         </form>
     )
 };
--- src/util/Input.jsx
+++ src/util/Input.jsx
@@ -3,7 +3,8 @@ import styles from './Input.module.css';
 
 const Input = React.forwardRef((props, ref) => {
     return (
-        <div className={styles.control}>
+        //<div className={styles.control}>
+        <div>
             <label htmlFor={props.input.id} className={styles.label}>{props.label}</label>
             <input ref={ref} id={props.input.id} required {...props.input} className={styles.input}/>
         </div>
